651ae30b4dc023941a12da6a02a5db1a15eb3f3d
FxWolfxe <ndmuench@comcast.net>
2021-07-26 06:04:33 -0400

    sapio collar locks itself when equiped

---
+++ Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/ThingComps/AutoLocker.cs
@@ -0,0 +1,20 @@
+﻿// AutoLocker.cs created by Iron Wolf for PawnmorpherMutagenesis on 07/26/2021 6:01 AM
+// last updated 07/26/2021  6:01 AM
+
+using RimWorld;
+using Verse;
+
+namespace PawnmorpherMutagenesis.ThingComps
+{
+    public class AutoLocker : ThingComp
+    {
+        public override void Notify_Equipped(Pawn pawn)
+        {
+            base.Notify_Equipped(pawn);
+            if (!(parent is Apparel apParent)) return; 
+            var ap = pawn.apparel;
+            ap?.Lock(apParent); 
+
+        }
+    }
+}
