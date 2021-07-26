c397614452f7b08e716efc89ce79cc16e8e37369
FxWolfxe <ndmuench@comcast.net>
2021-07-25 22:09:23 -0400

    created sapience collar

---
+++ Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/Hediffs/CollarHediff.cs
@@ -0,0 +1,33 @@
+﻿// CollarHediff.cs created by Iron Wolf for PawnmorpherMutagenesis on 07/25/2021 9:45 PM
+// last updated 07/25/2021  9:45 PM
+
+using System.Linq;
+using Pawnmorph;
+using Pawnmorph.Utilities;
+using PawnmorpherMutagenesis.ThingComps;
+using UnityEngine;
+using Verse;
+
+namespace PawnmorpherMutagenesis.Hediffs
+{
+    public class CollarHediff : HediffWithComps
+    {
+        private SapienceLevelPickerComp _comp;
+
+
+        private SapienceLevelPickerComp PickerComp
+        {
+            get
+            {
+                if (_comp == null)
+                    _comp = pawn.apparel?.WornApparel.MakeSafe()
+                                .Select(a => a.TryGetComp<SapienceLevelPickerComp>())
+                                .FirstOrDefault();
+
+                return _comp;
+            }
+        }
+
+        public override int CurStageIndex => Mathf.Min(def.stages.Count - 1, (int) (PickerComp?.CurLevel ?? SapienceLevel.Feral));
+    }
+}
---
+++ Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/ThingComps/SapienceLevelPickerComp.cs
@@ -0,0 +1,74 @@
+﻿// SapiencePickerComp.cs created by Iron Wolf for PawnmorpherMutagenesis on 07/25/2021 8:54 PM
+// last updated 07/25/2021  8:54 PM
+
+using System.Collections.Generic;
+using Pawnmorph;
+using Verse;
+
+namespace PawnmorpherMutagenesis.ThingComps
+{
+    public class SapienceLevelPickerComp : ThingComp
+    {
+        private Command_Action _cachedGizmo;
+
+        private SapienceLevel _curLevel;
+
+        private Gizmo[] _cachedArray;
+        public SapienceLevel CurLevel => _curLevel;
+
+
+        public override IEnumerable<Gizmo> CompGetWornGizmosExtra()
+        {
+            return CachedArray;
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref _curLevel, nameof(CurLevel));
+        }
+
+        private Command_Action CachedGizmo
+        {
+            get
+            {
+                if (_cachedGizmo == null)
+                    _cachedGizmo = new Command_Action
+                    {
+                        action = CycleActiveMode, defaultLabel = _curLevel.ToString().Translate(),
+                        defaultDesc = _curLevel.ToString().Translate() //TODO hookup gizmo graphics once yap makes them 
+                    };
+
+                return _cachedGizmo;
+            }
+        }
+
+        private Gizmo[] CachedArray
+        {
+            get
+            {
+                if (_cachedArray == null) _cachedArray = new[] {CachedGizmo};
+
+                return _cachedArray;
+            }
+        }
+
+        private void CycleActiveMode()
+        {
+            if (_curLevel == SapienceLevel.Feral)
+                _curLevel = SapienceLevel.Sapient;
+            else
+                _curLevel = (SapienceLevel) ((int) _curLevel + 1);
+
+            _cachedGizmo.defaultLabel = _curLevel.ToString().Translate();
+        }
+    }
+
+    public class SapienceLevelPickerCompProps : CompProperties
+    {
+        public SapienceLevelPickerCompProps()
+        {
+            compClass = typeof(SapienceLevelPickerComp);
+        }
+    }
+}
