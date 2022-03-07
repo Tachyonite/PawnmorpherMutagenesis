01a2e23565b230e99a5080f64cd66f0ac5502f96
FxWolfxe <ndmuench@comcast.net>
2022-03-07 09:55:14 -0500

    added more notes

---
+++ Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/DebugUtils/MutagenesisDebugUtils.cs
@@ -0,0 +1,14 @@
+﻿// MutagenesisDebugUtils.cs created by Iron Wolf for PawnmorpherMutagenesis on 03/07/2022 9:16 AM
+// last updated 03/07/2022  9:16 AM
+
+namespace PawnmorpherMutagenesis.DebugUtils
+
+{
+    public static class MutagenesisDebugUtils
+    {
+        private const string DEBUG_CATEGORY = "PM_Mutagenesis"; 
+
+
+
+    }
+}
