3c1d7e3c5cb75c3f5990ac858087eb931fb88c09
FxWolfxe <ndmuench@comcast.net>
2022-03-18 07:49:16 -0400

    work on outline of mutagenic conversion ritual

---
+++ Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/Rituals/OutcomeEffectWorker_MutagenicConversion.cs
@@ -0,0 +1,12 @@
+﻿// OutcomeEffectWorker_MutagenicConversion.cs created by Iron Wolf for PawnmorpherMutagenesis on 03/18/2022 7:20 AM
+// last updated 03/18/2022  7:20 AM
+
+using RimWorld;
+
+namespace PawnmorpherMutagenesis.Rituals
+{
+    public class OutcomeEffectWorker_MutagenicConversion : RitualOutcomeEffectWorker_FromQuality
+    {
+        //TODO 
+    }
+}
---
+++ Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/Rituals/Worker_MutagenicConversion.cs
@@ -0,0 +1,12 @@
+﻿// Worker_MutagenicConversion.cs created by Iron Wolf for PawnmorpherMutagenesis on 03/18/2022 7:18 AM
+// last updated 03/18/2022  7:18 AM
+
+using RimWorld;
+
+namespace PawnmorpherMutagenesis.Rituals
+{
+    public class Worker_MutagenicConversion : RitualBehaviorWorker
+    {
+        
+    }
+}
