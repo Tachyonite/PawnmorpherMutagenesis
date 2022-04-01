09410a30fb758cd69c502d566184bed74b6e5938
Micah <microbious101@gmail.com>
2022-04-01 23:57:58 +1300

    Finished role restriction mod extension

--- Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/HPatches/RoleLockedRecipePatches.cs
+++ Source/PawnmorpherMutagenesis/PawnmorpherMutagenesis/HPatches/RoleLockedRecipePatches.cs
@@ -3,25 +3,41 @@ using JetBrains.Annotations;
 using System.Collections.Generic;
 using RimWorld;
 using Verse;
+using Verse.AI;
 using PawnmorpherMutagenesis.DefExtensions;
 
 
 namespace PawnmorpherMutagenesis.HPatches
 {
+    [HarmonyPatch(typeof(Bill)), HarmonyPatch(nameof(Bill.PawnAllowedToStartAnew))]
     static class RoleLockedRecipePatches
     {
-        [HarmonyPatch(typeof(Bill)), HarmonyPatch(nameof(Bill.PawnAllowedToStartAnew)), HarmonyPostfix]
-        static bool CheckRoleRestrictionAbility(ref bool __result, Bill __instance, Pawn p)
+
+        [HarmonyPostfix]
+        static void CheckRoleRestrictionAbility(ref bool __result, Bill __instance, Pawn p)
         {
-            if (__instance.recipe.HasModExtension<RoleRestrictions>())
+            if (__result == false)
+            {  // nothing to do here
+                return;
+            }
+            RoleRestrictions extension = __instance.recipe.GetModExtension<RoleRestrictions>();
+            if (extension == null)
             {
-                if (p.Ideo.GetRole(p) == null || !p.Ideo.GetRole(p).def.defName.Equals(__instance.recipe.GetModExtension<RoleRestrictions>().role.defName))
+                extension = __instance.recipe.ProducedThingDef.GetModExtension<RoleRestrictions>();
+                if (extension == null)
                 {
-                    __result = false;
-                    return false;
+                    return;
                 }
             }
-            return true;
+            Precept_Role role = p.Ideo.GetRole(p);
+            if (role == null)
+            {
+                JobFailReason.Is($"Missing required role: {extension.role.label.CapitalizeFirst()}",__instance.Label);
+                __result = false;
+                return;
+            }
+            bool roleAllowed = role.def == extension.role;  // I'd make the extension have a list of roles that are allowed
+            __result = roleAllowed;
         }
     }
 }
